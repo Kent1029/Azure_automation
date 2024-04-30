44adec12e5ec5925a9f32c1ecf4c0ec48363291c
Kent Huang <y787896@gmail.com>
2024-04-30 10:55:32 +0800

    update

--- Storage_account/Expiration_Notice/HTML_message.py
+++ Storage_account/Expiration_Notice/HTML_message.py
@@ -11,30 +11,30 @@ message = """\
     <p>Dear user：<br><br>
 
     因應資安規範，以下是您將在三十日內即將到期的 Azure Firewall Policy，<br>
-    若尚有使用需求，請在到期日之前至 ITSR 網站<a href="http://whqeform.wistron.com/eWHQ/P00/FM_FormQuery.aspx">汐止/內湖</a> 或 <a href="http://wiheform.wistron.com/eWIH/P00/FM_Mypage.aspx">新竹</a> 重新申請，<br>
+    若尚有使用需求，請在到期日之前至 ITSR 網站<a href="[]">汐止/內湖</a> 或 <a href="[]">新竹</a> 重新申請，<br>
     (ITSR 網站->表單查詢->SR Query-> 按申請單號查詢)<br>  
     若無使用需求，也請填 ITSR 刪除。<br>
     如若到期日之前未收到您的申請或回覆，依資安規範將將先關閉您的 Azure Firewall Policy。<br><br>
 
     如有任何問題，您可以聯繫 Azure Firewall Admin：<br>
     <ul>
-      <li><a href="mailto:kent_huang@wistron.com">Kent Huang</a></li>
-      <li><a href="mailto:yuna_lin@wistron.com">Yuna Lin</a></li>
-      <li><a href="mailto:keynes_lee@wistron.com">Keynes Lee</a></li>
+      <li><a href="mailto:kent@mail.com">Kent </a></li>
+      <li><a href="mailto:yuta@mail.com">Yuta </a></li>
+      <li><a href="mailto:ken@mail.com">Ken </a></li>
     </ul>
     
     -<br>
     In response to cybersecurity regulations, below is your Azure Firewall Policy that will expire within thirty days. <br>
-    If there is still a need for use, please reapply on the ITSR website <a href="http://whqeform.wistron.com/eWHQ/P00/FM_FormQuery.aspx">Xizhi/Neihu</a> or <a href="http://wiheform.wistron.com/eWIH/P00/FM_Mypage.aspx">Hsinchu</a> before the expiration date. <br>
+    If there is still a need for use, please reapply on the ITSR website <a href="[]">Xizhi/Neihu</a> or <a href="[]">Hsinchu</a> before the expiration date. <br>
     If there is no need for use, please fill out the ITSR deletion form. <br>
 
     When we do not receive your application or response before the expiration date, in accordance with cybersecurity regulations, we will first disable your Azure Firewall Policy.<br><br>
 
     When you have any questions, you can contact the Azure Firewall Admins,: <br>
     <ul>
-      <li><a href="mailto:kent_huang@wistron.com">Kent Huang</a></li>
-      <li><a href="mailto:yuna_lin@wistron.com">Yuna Lin</a></li>
-      <li><a href="mailto:keynes_lee@wistron.com">Keynes Lee</a></li>
+      <li><a href="mailto:kent@mail.com">Kent </a></li>
+      <li><a href="mailto:yuta@mail.com">Yuta </a></li>
+      <li><a href="mailto:ken@mail.com">Ken </a></li>
     </ul>
     </p>
 
